cf6630431f5986f23174e7d44772867c2eed3c44
bkgithello <birukdark@gmail.com>
2025-10-09 09:46:16 +0300

    Implement the Menu section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
 import About from './components/About';
+import Art from './components/Art';
+import Menu from './components/Menu';
 
 gsap.registerPlugin(ScrollTrigger, ScrollTrigger);
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
     <Hero/>
     <Cocktails/>
     <About/>
+    <Art/>
+    <Menu/>
    </main>
   )
 }
